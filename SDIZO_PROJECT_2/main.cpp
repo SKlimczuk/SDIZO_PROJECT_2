d36c8f9cd6c32055f25ec7dfce464ad0462b59de
SZYMON <klimczukszymon@gmail.com>
2018-05-08 16:36:59 +0200

    created second project file

---
+++ SDIZO_PROJECT_2/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  SDIZO_PROJECT_2
+//
+//  Created by Szymon Klimczuk on 08.05.2018.
+//  Copyright © 2018 Szymon Klimczuk. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    
+    return 0;
+}
